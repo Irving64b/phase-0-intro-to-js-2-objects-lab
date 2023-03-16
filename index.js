b2cfb7c2fb49f72d108f5f471a51910335030c2f
Irving Medina <irving.64@hotmail.com>
2023-03-16 13:12:49 -0400

    complete objects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "Someone",
+    streeAdress: "23 Lives Here",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee, "name": "Sam", "streetAddress": "11 Broadway"}
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee[key]}
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
